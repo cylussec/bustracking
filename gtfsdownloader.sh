7d37fd4d5790f9a26b1370442a77141bf3f88596
Brian Seel <brian.seel@gmail.com>
2019-12-29 14:22:34 -0500

    Dealing with non ascii characters that sometimes were in the stop_times.txt

--- gtfsdownloader.sh
+++ gtfsdownloader.sh
@@ -2,3 +2,4 @@ rm -f /opt/bitnami/logstash/mta-transit-data/google_transit.zip
 wget -O /tmp/google_transit.zip https://s3.amazonaws.com/mdotmta-gtfs/google_transit.zip
 unzip -o /tmp/google_transit.zip -d /opt/bitnami/logstash/mta-transit-data/
 rm -f /tmp/*_data
+sudo perl -i.bak -pe 's/[^[:ascii:]]//g' /opt/bitnami/logstash/mta-transit-data/stop_times.txt
