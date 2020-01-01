64251c8f6b8aa0f6cf199999fbd1c750fb904652
Brian Seel <brian.seel@gmail.com>
2019-12-31 19:27:19 -0500

    Fixing issue with folder removal

--- gtfsdownloader.sh
+++ gtfsdownloader.sh
@@ -1,5 +1,5 @@
 rm -f /opt/bitnami/logstash/mta-transit-data/google_transit.zip
-rm -f /opt/bitnami/logstash/mta-transit-data/google_transit/
+rm -rf /opt/bitnami/logstash/mta-transit-data/google_transit/
 rm -f /tmp/google_transit.zip
 wget -O /tmp/google_transit.zip https://s3.amazonaws.com/mdotmta-gtfs/google_transit.zip
 unzip -o /tmp/google_transit.zip -d /opt/bitnami/logstash/mta-transit-data/
